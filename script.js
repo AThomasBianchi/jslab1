b1971ae8ea1e3da8579850861bd4d8b48ddd03f7
Tom Bianchi <atbianchi@gmail.com>
2018-01-29 11:11:27 -0500

    Monday changes

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-var grantLives = 3;
+var grantLives = 2;
 var grantHealth = 10;
 var userHealth = 40;
 var userWins = 0;
+var userName;                                                       // userName is undefined until startGame() is called
 
-var wanttoplay = prompt("Do you want to play?");                  //ask the user if they want to play
-if (wanttoplay === "yes") {                                       // play, if they want to
-  var userName = prompt("What is your name?");                    // get the user name
-  console.log("Username: " + userName);                           // print the user name to confirm
+function getDamage() {                                              //generates damage between 1 and 5
+  return Math.floor(Math.random() * 5 + 1);
+}
+
+function startGame() {
+  var wanttoplay = prompt("Do you want to play?");                  //ask the user if they want to play
+  if (wanttoplay === "yes") {                                       // play, if they want to
+    userName = prompt("What is your name?");                        // get the user name
+    console.log("Username: " + userName);
+    startCombat();
+  }
+}
+
+function startCombat() {
   while (userHealth > 0 && grantLives >= 0 && grantHealth > 0) {  // while everyone is alive, do this
     console.log(userName + " has " + userHealth + " health left."); // log user health
     console.log("Grant the Mighty Chicken has " + grantHealth + " health left."); // log Grant health
-    var userDamage = Math.floor(Math.random() * 2 + 1);           // generate user damage
-    var grantDamage = Math.floor(Math.random() * 2 + 1);          // generate grant damage
-    userHealth -= userDamage;                                     // subract damage from the user health
-    grantHealth -= grantDamage;                                   // subract damage from grant's health
+    var attackAnswer = prompt("Do you want to attack or quit?");
+    if (attackAnswer === "quit") {
+      break;
+    }
+
+    userHealth -= getDamage();                                    // subract damage from user's health
+    grantHealth -= getDamage();                                   // subract damage from grant's health
                                                                   // evaluate if everyone is still alive below
     if (grantHealth <= 0 && userHealth <= 0 && grantLives < 1) {  // did you both die?
       console.log("You both died.");                              // then do this
@@ -24,13 +38,15 @@ if (wanttoplay === "yes") {                                       // play, if th
         console.log("But Grant has " + grantLives + " extra lives left");
         grantLives--;                                             // use an extra life
         grantHealth = 10;                                         // reset Grant's health
-      } else {
-        console.log("You have defeated Grant.");                  // no extra lives
-        console.log("You have " + userWins + " wins.");           // game over, you win
+      } else {                                                    // no extra lives
+        console.log("You have defeated Grant.");
+        console.log("You are alive, Grant is dead, the world is cruel.");           // game over, you win
       }
-    } else if (userHealth <= 0) {                                 // user dies, bummer
+    } else if (userHealth <= 0) {                                 // user dies, you lose
       console.log("You died. Bummer.");
       console.log("Grant wins. Good for Grant.");
     }
   }
 }
+
+startGame();
