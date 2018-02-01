ff8bd4b52d1ad1d4ee3c59d1d0a6ee3da62929f5
Tom Bianchi <atbianchi@gmail.com>
2018-02-01 00:05:25 -0500

    Functional, but not pretty

--- oopscript.js
+++ oopscript.js
@@ -21,56 +21,56 @@ var grant = {
 }
 
 function startGame() {
-  var wanttoplay = prompt("Do you want to play?");                  //ask the user if they want to play
-  if (wanttoplay.toLowerCase() === "yes") {                                       // play, if they want to
-    user.name = prompt("What is your name?");                        // get the user name
+  var wanttoplay = prompt("Do you want to play?");
+  if (wanttoplay.toLowerCase() === "yes") {
+    user.name = prompt("What is your name?");
     console.log("Username: " + user.name);
     startCombat();
   }
 }
 
 function dealDamage() {
-  user.health -= grant.generateAttackDamage();                                    // subract damage from user's health
-  grant.health -= user.generateAttackDamage();
+  user.health -= grant.generateAttackDamage(); // subract damage from user's health
+  grant.health -= user.generateAttackDamage(); // subtract damage from Grant's health
 }
 
 function logHealth() {
-  console.log(user.name + " has " + user.health + " health left.");
-  console.log("Grant the Mighty Chicken has " + grant.health + " health left.");
+  console.log(user.name + " has " + user.health + " health left."); // log the user's health
+  console.log("Grant the Mighty Chicken has " + grant.health + " health left."); // log Grant's health to the console
 }
 
 function startCombat() {
   while (user.health > 0 && user.wins < 3 && grant.health > 0) {
-    logHealth();
-    var attackAnswer = prompt("Do you want to attack or heal or quit?");
-    if (attackAnswer === "quit") {
+    logHealth(); // log Grant's and user's health
+    var attackAnswer = prompt("Do you want to attack or heal or quit?"); // user chooses what to do
+    if (attackAnswer === "quit") { // quit option
       break;
-    } else if (attackAnswer === "heal") {
-      console.log("You choose to heal yourself.");
-      if (user.healsRemaining > 0) {
-        user.heal();
-        console.log("Your health is now " + user.health);
+    } else if (attackAnswer === "heal") { // heal option
+      console.log("Physician, heal thyself.");
+      if (user.healsRemaining > 0) { // does user have heals left?
+        user.heal(); // heal
       } else {
-        console.log("You can't heal anymore.");
+        console.log("You can't heal anymore."); // tell them they can't heal anymore
+        continue;
       }
-      continue;
-    }
+
 
     dealDamage();
                                                                   // evaluate if everyone is still alive below
     if (grant.health <= 0 && user.health <= 0 && user.wins === 2) {  // did you both die?
-      console.log("You both died.");                              // then do this
-    } else if (grant.health <= 0) {                                // did Grant die?
+      console.log("You both died.");
+    } else if (grant.health <= 0) { // did Grant die?
       console.log("Grant Died");
       user.wins++;
+      console.log("You have " + user.wins + " wins.");
       if (user.wins < 3) { // does he have any extra lives?
-        console.log("But Grant has " + (3 - user.wins) + " extra lives left")                                            // use an extra life
-        grant.health = 10;                                         // reset Grant's health
-      } else {                                                    // no extra lives
+        console.log("But Grant has " + (3 - user.wins) + " extra lives left");                                            // use an extra life
+        grant.health = 10; // reset Grant's health
+      } else { // If Grant has no extra lives remaining
         console.log("You have defeated Grant.");
-        console.log("You are alive, Grant is dead, the world is cruel.");           // game over, you win
+        console.log("You are alive, Grant is dead, the world is cruel."); // game over, you win
       }
-    } else if (user.health <= 0) {                                 // user dies, you lose
+    } else if (user.health <= 0) { // user dies, you lose
       console.log("You died. Bummer.");
       console.log("Grant wins. Good for Grant.");
     }
